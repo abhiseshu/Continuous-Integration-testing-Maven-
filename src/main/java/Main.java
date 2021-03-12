5409e717d5bd3fd637bd262f2295ac99fe9dcf04
Adurti Devi Abhiseshu <dead20@student.bth.se>
2021-03-12 12:33:31 +0100

    Add test case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public int result;
+
+    Main(){
+        result = 0;
+    }
+
+    public int add(int a,int b){
+        result = a+b+1;
+        return result;
+    }
 }
--- src/test/java/TestMainByUnit.java
+++ src/test/java/TestMainByUnit.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestMainByUnit {
+
+    Main main;
+
+    @Before
+    public void testSetup(){
+        main = new Main();
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(13,main.add(10,3));
+    }
+
 }
