d4f7b222b30492715c86fe7ca0f12ce211f766d9
Govardhanchowdary7456 <goar20@student.bth.se>
2021-03-13 23:49:33 +0100

    Unit Test cases optimized

--- src/test/java/TestMainByUnit.java
+++ src/test/java/TestMainByUnit.java
@@ -1,42 +1,79 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+
+//Start of the Unit Test Script
 public class TestMainByUnit {
 
+    //Instances declaration
     Main main;
 
+    //resusable code variables
+    private int firstVar = 10;
+    private int secondVar = 3;
+
+    //Runs before every test case
     @Before
-    public void testSetup(){
+    public void testSetup() {
         main = new Main();
     }
 
-    @Test
-    public void testAdd(){
-        assertEquals(13,main.add(10,3));
+    //Runs after every test case
+    @After
+    public void testTearDown() {
+        main = null;
     }
 
-    @Test
-    public void testSub(){ assertEquals(7,main.sub(10,3));}
+//-------------- UNIT TEST CASES ------------
 
+    //tests the add function
     @Test
-    public void testMul() {assertEquals(30, main.mult(10,3));}
-
-    @Test
-    public void testdiv() {assertEquals(3,main.div(10,3));}
-
-    @Test
-    public void testgetResult(){assertNotEquals(null, main.getResult());}
-
-    @Test
-    public void testsetResult(){
-        assertEquals(10,main.setResult(10));
+    public void testAdd() {
+        int expVal = 13;
+        assertEquals(expVal, main.add(firstVar, secondVar));
     }
 
+    //tests the sub function
     @Test
-    public void testClear(){
-        assertEquals(0,main.clear());
+    public void testSub() {
+        int expVal = 7;
+        assertEquals(expVal, main.sub(firstVar, secondVar));
+    }
+
+    //tests the mult function
+    @Test
+    public void testMul() {
+        int expVal = 30;
+        assertEquals(expVal, main.mult(firstVar, secondVar));
+    }
+
+    //tests the div function
+    @Test
+    public void testdiv() {
+        int expVal = 3;
+        assertEquals(expVal, main.div(firstVar, secondVar));
+    }
+
+    //tests the getResult function
+    @Test
+    public void testgetResult() {
+        assertNotEquals(null, main.getResult());
+    }
+
+    //tests the set Result function
+    @Test
+    public void testsetResult() {
+        assertEquals(firstVar, main.setResult(firstVar));
+    }
+
+    //tests the clear function
+    @Test
+    public void testClear() {
+        int expVal = 0;
+        assertEquals(expVal, main.clear());
     }
 }
